2285d77390e7fe2010cac8f22cc2ec784c1fa935
stjoo0925 <stjoo0925@gmail.com>
2025-03-21 13:19:32 +0900

    feat : 파비콘 적용됨

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,12 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'SoontaeJoo\'s Portfolio',
   description: '주순태의 개발자 포트폴리오 입니다',
+  manifest: '/icons/site.webmanifest',
+  icons: {
+    icon: '/icons/favicon.ico',
+    shortcut: '/icons/favicon.ico',
+    apple: '/icons/apple-touch-icon.png',
+  },
 }
 export default function RootLayout({
   children,
