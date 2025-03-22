abb2ea2eb3eb0d50027ff47fbc0c9ed57c6eeee8
Stjoo0925 <stjoo0925@gmail.com>
2025-03-22 22:04:39 +0900

    feat : 메타데이터 개선

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,30 @@ export const metadata: Metadata = {
     shortcut: '/icons/favicon.ico',
     apple: '/icons/apple-touch-icon.png',
   },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  openGraph: {
+    title: 'SoontaeJoo\'s Portfolio',
+    description: '주순태의 개발자 포트폴리오 입니다',
+    type: 'website',
+    locale: 'ko_KR',
+    images: [
+      {
+        url: '/images/og-image.webp',
+        width: 1200,
+        height: 630,
+        alt: '포트폴리오 썸네일',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'SoontaeJoo\'s Portfolio',
+    description: '주순태의 개발자 포트폴리오 입니다',
+    images: ['/images/og-image.webp'],
+  },
 }
 export default function RootLayout({
   children,
